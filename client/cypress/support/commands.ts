6e5f6b2dc0115b855b711469f2a6a215579b1710
Leandro Reis <leohbr@gmail.com>
2024-05-28 15:11:30 -0300

    clear tests

--- client/cypress/support/commands.ts
+++ client/cypress/support/commands.ts
@@ -36,54 +36,5 @@
 //   }
 // }
 
-import Prisma from '@prisma/client';
 
-declare global {
-  namespace Cypress {
-    interface Chainable {
-      login: (username: string, password: string) => {};
-      getByCy<E extends Node = HTMLElement>(
-        cyId: string
-      ): Cypress.Chainable<JQuery<E>>;
-      resetDb(): void;
-      createHero(): Cypress.Chainable<Prisma.Hero>;
-      deleteHero(id: number): void;
-    }
-  }
-}
 
-Cypress.Commands.add('login', (username: string, password: string) => {
-  cy.session(`login-${username}`, () => {
-    cy.visit('/');
-    cy.get('button').contains('Login').click();
-    cy.get('input[type="email"]').type(username);
-    cy.get('input[type="password"]').type(password);
-    cy.get('button').contains('Sign in').click();
-    cy.contains('button', 'Logout', { log: false }).should('be.visible');
-  });
-});
-
-Cypress.Commands.add('getByCy', (cyId: string) => {
-  return cy.get(`[data-cy='${cyId}'`);
-});
-
-Cypress.Commands.add('resetDb', () => {
-  cy.exec(
-    'npx prisma migrate reset --skip-seed -f && npx prisma db push && npx prisma db seed'
-  );
-});
-
-Cypress.Commands.add('createHero', () => {
-  return cy.task('createHero').then((newHero: any) => {
-    delete newHero.createdAt;
-    delete newHero.updatedAt;
-    delete newHero.avatar;
-    return cy.wrap<Prisma.Hero>(newHero).as('newHero');
-  });
-});
-
-Cypress.Commands.add('deleteHero', (id: number) => {
-  cy.task('deleteHero', id);
-});
-
-export {};
